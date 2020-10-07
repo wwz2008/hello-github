d647221ab24cb195617fe71c1963b2a8544a62a3
wwz2008 <59471648+wwz2008@users.noreply.github.com>
2020-10-07 12:24:50 +0800

    a c++ program
    use for output the digits of pi from the pilish text.

---
+++ pi.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	freopen("CadaeicCadenza.txt","r",stdin);
+	freopen("pi.txt","w",stdout);
+	char c,t='\0';
+	int cnt=0;
+	while((c=getchar())!=EOF)
+	{
+		if(isalpha(c))	cnt++;
+		else
+		{
+			if(isalpha(t))	cout<<(cnt==10?0:cnt);
+			cnt=0;
+		}
+		t=c;
+	} 
+    return 0;
+}
+
